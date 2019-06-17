c181effc12f8e894434f553d3c44c4a76db89352
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2019-06-17 08:25:21 -0300

    Heroku - SecretKey and Debug

--- joao_carlos_cabeleireiro/settings.py
+++ joao_carlos_cabeleireiro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3*637n%0@atv!$j#1*&z@q0jb7-y7#-@rg52ny8fxh(2%a(#8z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
